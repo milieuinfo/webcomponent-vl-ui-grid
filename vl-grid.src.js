65f3d544a9597015fa224fcf4a95ca49b2e8933c
Tom Coemans <tom.coemans@gmail.com>
2019-06-24 08:09:23 +0200

    demo link toegevoegd

--- vl-grid.src.js
+++ vl-grid.src.js
@@ -30,7 +30,7 @@ export class VlGrid extends NativeVlElement(HTMLDivElement) {
  * VlColumn
  * @class
  * @classdesc
- * De parent van een VlColumn is altijd een VlGrid.
+ * De parent van een VlColumn is altijd een VlGrid. <a href="demo/vl-grid.html">Demo</a>.
  * @extends VlElement
  * @property {number} size - De teller van de verdeling van grote scherm.
  * @property {number} max-size - De noemer van de verdeling van grote scherm.
