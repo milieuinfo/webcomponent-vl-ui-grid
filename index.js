56356040468ebbf01c38903874469457b48a8b7c
coemans <tom.coemans@gmail.com>
2020-06-03 13:09:28 +0200

    Introductie vl-ui-demo en ESlint (#85)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = { 
-    Test: {
-        VlRegion: require('./test/e2e/components/vl-grid').VlRegion,
-        VlLayout: require('./test/e2e/components/vl-grid').VlLayout,
-        VlGrid: require('./test/e2e/components/vl-grid').VlGrid,
-        VlColumn: require('./test/e2e/components/vl-grid').VlColumn
-    }
+module.exports = {
+  Test: {
+    VlRegion: require('./test/e2e/components/vl-grid').VlRegion,
+    VlLayout: require('./test/e2e/components/vl-grid').VlLayout,
+    VlGrid: require('./test/e2e/components/vl-grid').VlGrid,
+    VlColumn: require('./test/e2e/components/vl-grid').VlColumn,
+  },
 };
