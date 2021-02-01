0b2241c668be8137a0d6f76e160e8fc786a4e771
coemans <tom.coemans@vlaanderen.be>
2021-02-01 18:26:00 +0100

    update vl-ui-core dependency (#112)

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,1 @@
-module.exports = {
-  VlRegion: require('../dist/vl-grid.src.js').VlRegion,
-  VlLayout: require('../dist/vl-grid.src.js').VlLayout,
-  VlGrid: require('../dist/vl-grid.src.js').VlGrid,
-  VlColumn: require('../dist/vl-grid.src.js').VlColumn,
-};
+export * from '../dist/vl-grid.src.js';
